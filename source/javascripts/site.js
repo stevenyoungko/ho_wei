d914679afc39f883b544d00dae954443ef384d32
柯炫仰 <kexuanyang@kexuanyangde-MacBook-Air.local>
2018-04-27 17:13:12 +0800

    [add] added modify

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -14,46 +14,73 @@ $(document).ready(function() {
         $('#js-Menu').removeClass('stick-to-top')
       }
     },
+    
 
-     afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {
-       console.log( slideAnchor, slideIndex)
-       if(anchorLink === 'secondPage' && slideIndex === 0){
-         $('.second-bg-image .fp-prev').addClass('remove-back')
-       }
-       if(anchorLink === 'secondPage' && slideIndex === 1){
-         $('.second-bg-image .fp-prev').removeClass('remove-back')  
-       }
-       if(anchorLink === 'secondPage' && slideIndex === 1){
-        $('.second-bg-image .fp-next').addClass('remove-next') 
-       }
-       if(anchorLink === 'secondPage' && slideIndex === 0){
-        $('.second-bg-image .fp-next').removeClass('remove-next')
-       }
+    afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex) {
+      console.log( slideAnchor, slideIndex)  
+      if(anchorLink === 'secondPage' && slideIndex === 0){
+        $('.second .fp-prev').addClass('remove-back')
+      }
+      if(anchorLink === 'secondPage' && slideIndex === 1){
+        $('.second .fp-prev').removeClass('remove-back')  
+      }
+      if(anchorLink === 'secondPage' && slideIndex === 1){
+        $('.second .fp-next').addClass('remove-next') 
+      }
+      if(anchorLink === 'secondPage' && slideIndex === 0){
+        $('.second .fp-next').removeClass('remove-next')
+      }
 
-       if(anchorLink === 'thirdPage' && slideIndex === 0){
-        $('.third-page .fp-prev').addClass('remove-back')
-       }
-       if(anchorLink === 'thirdPage' && slideIndex === 0){
-        $('.third-page .fp-next.remove-next').removeClass('change-next')  
-       }
-       if(anchorLink === 'thirdPage' && slideIndex === 0){
-        $('.third-page .fp-next').removeClass('remove-next')
-       }
-       if(anchorLink === 'thirdPage' && slideIndex === 1){
-        $('.third-page .fp-prev').removeClass('remove-back')  
-       }
-       if(anchorLink === 'thirdPage' && slideIndex === 1){
-        $('.third-page .fp-next').addClass('remove-next') 
-       }
-       if(anchorLink === 'thirdPage' && slideIndex === 1){
-        $('.third-page .fp-next.remove-next').addClass('change-next')  
-       }
-       if(anchorLink === 'thirdPage' && slideIndex === 2){
-        $('.third-page .fp-next').addClass('remove-next') 
-       }
-       if(anchorLink === 'thirdPage' && slideIndex === 2){
-        $('.third-page .fp-next.remove-next').removeClass('change-next')  
-       }
-     }
+      
+      if(anchorLink === 'thirdPage' && slideIndex === 0){
+        $('.third .fp-prev').addClass('remove-back')
+      }
+      if(anchorLink === 'thirdPage' && slideIndex === 0){
+        $('.third .fp-next.remove-next').removeClass('change-next')  
+      }
+      if(anchorLink === 'thirdPage' && slideIndex === 0){
+        $('.third .fp-next').removeClass('remove-next')
+      }
+      if(anchorLink === 'thirdPage' && slideIndex === 1){
+        $('.third .fp-prev').removeClass('remove-back')  
+      }
+      if(anchorLink === 'thirdPage' && slideIndex === 1){
+        $('.third .fp-next').addClass('remove-next') 
+      }
+      if(anchorLink === 'thirdPage' && slideIndex === 1){
+        $('.third .fp-next.remove-next').addClass('change-next')  
+      }
+      if(anchorLink === 'thirdPage' && slideIndex === 2){
+        $('.third .fp-next').addClass('remove-next') 
+      }
+      if(anchorLink === 'thirdPage' && slideIndex === 2){
+        $('.third .fp-next.remove-next').removeClass('change-next')  
+      }
+
+
+      if(anchorLink === 'fourthPage' && slideIndex === 0){
+        $('.fourth .fp-prev').addClass('remove-back')
+      }
+      if(anchorLink === 'fourthPage' && slideIndex === 1){
+        $('.fourth .fp-prev').addClass('change-back')
+      }
+      else{
+        $('.fourth .fp-prev').removeClass('change-back')
+      }
+      if(anchorLink === 'fourthPage' && slideIndex === 2){
+        $('.fourth .fp-next').addClass('remove-next')
+      }
+      else{
+        $('.fourth .fp-next').removeClass('remove-next')
+      }
+      if(anchorLink === 'fourthPage' && slideIndex === 2){
+        $('.fourth .fp-next.remove-next').addClass('change-next')
+      }
+      if(anchorLink === 'fourthPage' && slideIndex === 2){
+        $('.fourth .fp-prev').removeClass('remove-back')
+      } 
+    }
   });
 });
+
+

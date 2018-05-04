7fdf9c482ec3eb5d7409ad7687d75cbfc61d5160
柯炫仰 <kexuanyang@kexuanyangde-MacBook-Air.local>
2018-05-04 21:33:05 +0800

    [add] added modify-2

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,10 +1,11 @@
 // This is where it all goes :)
 //= require test.js
 $(document).ready(function() {
+  
   $('#fullpage').fullpage({
     anchors:['firstPage', 'secondPage', 'thirdPage','fourthPage','fifthPage','sixthPage','seventhPage'],
     menu: '#js-Menu',
-    // loopHorizontal: false¥
+    slidesNavigation: true,
     afterLoad: function(anchorLink, index) {
       if(anchorLink !== 'firstPage') {
         $('#js-Menu').addClass('stick-to-top')
@@ -12,6 +13,28 @@ $(document).ready(function() {
 
       if(anchorLink === 'firstPage') {
         $('#js-Menu').removeClass('stick-to-top')
+      }
+
+      //if (anchorLink === "fifthPage") {
+        //$("#js-Menu").addClass("disappear");
+      //}
+
+      //if (anchorLink === "fifthPage") {
+        //$(".footer").addClass("disappear1");
+      //}
+
+      if(anchorLink === 'firstPage') {
+        $(".logo").addClass("remove-logo");
+      }
+      else {
+        $(".logo").removeClass("remove-logo");
+      }
+
+      if(anchorLink !== 'sixthPage') {
+        $(".fp-slidesNav").removeClass("fp-slidesNav");
+      }
+      if(anchorLink === 'sixthPage') {
+        $(".fp-slidesNav").addClass("fp-slidesNav");
       }
     },
     
@@ -78,9 +101,36 @@ $(document).ready(function() {
       }
       if(anchorLink === 'fourthPage' && slideIndex === 2){
         $('.fourth .fp-prev').removeClass('remove-back')
-      } 
+      }
+
+
+      if(anchorLink === "sixthPage" && slideIndex === 0) {
+        $(".sixth .fp-controlArrow").addClass("remove-back");
+      }
     }
   });
 });
 
 
+var contactPanel,
+    contactPanelCloseButton;
+
+
+$(document).ready(function(){
+  contactPanel = $(".popup-contact");
+  contactPanelCloseButton = contactPanel.find(".popup-form-close");
+
+  contactPanelOpenButton = $('[data-menuanchor="seventhPage"]');
+  contactPanelOpenButton.on('click', openContactPanel);
+  contactPanelCloseButton.on("click", closeContactPanel);
+});
+
+var openContactPanel = function() {
+  console.log('open')
+  contactPanel.addClass("is-active");
+};
+
+var closeContactPanel = function() {
+  console.log("close")
+  contactPanel.removeClass("is-active");
+};
